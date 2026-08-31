8d6a0d6882d237554e59856c45072fab31ba31ea
Richard <richardbassani666@gmail.com>
2026-08-31 11:58:46 +0200

    Update clear.bat

--- clear.bat
+++ clear.bat
@@ -1,21 +1,18 @@
 @echo off
+setlocal
 
-:: 1. Rimuove la documentazione e lo script clear per Linux
-del /q README.md LICENSE.md clear.sh
+del /q README.md LICENSE.md clear.sh 2>nul
 
-:: 2. Si sposta fuori, rinomina dcc.hpp in DCC e rientra nella nuova cartella
 cd ..
 if exist dcc.hpp (
     ren dcc.hpp DCC
     cd DCC
 )
 
-:: 3. Ora che la cartella è rinominata, lancia build.bat passando il nome di questo file da eliminare
 if exist build.bat (
     start "" build.bat clear.bat
     exit
 ) else (
-    echo Setup completed! 'build.bat' not found.
     del "%~f0"
     pause
 )
