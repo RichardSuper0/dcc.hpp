da6ad78af0c6f1cc491a36f230ed8c13b9f2c343
Richard <richardbassani666@gmail.com>
2026-08-31 11:58:36 +0200

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# 1. Rimuove la documentazione e lo script clear per Windows
 rm -f README.md LICENSE.md clear.bat
 
-# 2. Si sposta fuori, rinomina dcc.hpp in DCC e rientra nella nuova cartella
 cd ..
 if [ -d "dcc.hpp" ]; then
     mv dcc.hpp DCC
     cd DCC
 fi
 
-# 3. Ora che la cartella è rinominata, rende eseguibile build.sh e lo avvia
 if [ -f "build.sh" ]; then
     chmod +x build.sh
     exec ./build.sh clear.sh
 else
-    echo "Setup completed! 'build.sh' not found."
     rm -- "$0"
 fi
